bee1c08dac8093aec84b35b75f7518fcf457ff29
Saghar Hosseinmardi <saghar.h.mardi@gmail.com>
2024-02-27 17:47:14 +0000

    update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import "../node_modules/font-awesome/css/font-awesome.min.css";
+import "../node_modules/elegant-icons/style.css";
+import "../node_modules/et-line/style.css";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "../node_modules/bootstrap/dist/js/bootstrap.js";
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
